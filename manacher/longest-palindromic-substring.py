3db49fc0c210a94f76cdd6b62d0ec41656a4dc4e
Chambertin Zhang <chamberlainzhang0821@gmail.com>
2026-08-19 16:00:27 -0700

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- manacher/longest-palindromic-substring.py
+++ manacher/longest-palindromic-substring.py
@@ -2,13 +2,22 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) <= 1:
             return s
-        
-        Max_Len=1
-        Max_Str=s[0]
-        for i in range(len(s)-1):
-            for j in range(i+1,len(s)):
-                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
-                    Max_Len = j-i+1
-                    Max_Str = s[i:j+1]
 
-        return Max_Str
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        max_str = s[0]
+
+        for i in range(len(s) - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
