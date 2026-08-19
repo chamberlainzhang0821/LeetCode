d687b42b06e93e3e1f71acd8c27773f731285a69
Chambertin Zhang <chamberlainzhang0821@gmail.com>
2026-08-19 16:02:35 -0700

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- two-pointers/longest-palindromic-substring.py
+++ two-pointers/longest-palindromic-substring.py
@@ -2,22 +2,22 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) <= 1:
             return s
-
-        def expand_from_center(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return s[left + 1:right]
-
-        max_str = s[0]
-
-        for i in range(len(s) - 1):
-            odd = expand_from_center(i, i)
-            even = expand_from_center(i, i + 1)
-
-            if len(odd) > len(max_str):
-                max_str = odd
-            if len(even) > len(max_str):
-                max_str = even
-
-        return max_str
+        
+        Max_Len=1
+        Max_Str=s[0]
+        s = '#' + '#'.join(s) + '#'
+        dp = [0 for _ in range(len(s))]
+        center = 0
+        right = 0
+        for i in range(len(s)):
+            if i < right:
+                dp[i] = min(right-i, dp[2*center-i])
+            while i-dp[i]-1 >= 0 and i+dp[i]+1 < len(s) and s[i-dp[i]-1] == s[i+dp[i]+1]:
+                dp[i] += 1
+            if i+dp[i] > right:
+                center = i
+                right = i+dp[i]
+            if dp[i] > Max_Len:
+                Max_Len = dp[i]
+                Max_Str = s[i-dp[i]:i+dp[i]+1].replace('#','')
+        return Max_Str
