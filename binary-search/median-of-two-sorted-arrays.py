6e786f952dfea9d2a1b05d73a30669ea795611f9
Chambertin Zhang <chamberlainzhang0821@gmail.com>
2026-08-18 21:05:43 -0700

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- binary-search/median-of-two-sorted-arrays.py
+++ binary-search/median-of-two-sorted-arrays.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = nums1 + nuns2
+        merged = nums1 + nums2
 
         merged.sort()
 
